76d356ed13bd7dda4822f40e92195521aee1468c
flarnrules <breifenberg11@gmail.com>
2023-06-05 20:53:54 -0500

    write more stuff in png_to_gif.py regarding file retrieval (how is this word spelled

--- scripts/png_to_gif.py
+++ scripts/png_to_gif.py
@@ -8,5 +8,9 @@ image_folder = "place your image folder here"
 # This creates the base name of the folder so the script can work with it
 folder_name = os.path.basename(image_folder.strip('/'))
 
-# Sorts the files based on their number, in ascending order (make sure all pngs are named as integers like 1, 2 or 001, 002)
+# Sorts the files based on their number, in ascending order 
+# Make sure all pngs are named as integers like 1, 2 or 001, 002
+file_list = sorted(glob.glob(f"{image_folder}/*.png"))
+
+# to be continued
 
